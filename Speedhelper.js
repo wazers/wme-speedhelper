2f2f8bbfed72b4e66e53c83d5bafe0dd3ebb5508
Dave Alverson <daveacincy.waze@gmail.com>
2024-03-01 16:44:50 -0500

    fix Panama entry

--- Speedhelper.js
+++ Speedhelper.js
@@ -4,7 +4,7 @@
 // @namespace      broosgert@gmail.com
 // @grant          none
 // @grant          GM_info
-// @version        0.12.5
+// @version        0.12.6
 // @include 	     /^https:\/\/(www|beta)\.waze\.com\/(?!user\/)(.{2,6}\/)?editor.*$/
 // @exclude        https://www.waze.com/user/*editor/*
 // @exclude        https://www.waze.com/*/user/*editor/*
@@ -26,11 +26,10 @@ const ScriptName = GM_info.script.name;
 const ScriptVersion = GM_info.script.version;
 
 let ChangeLog = "WME SpeedHelper has been updated to " + ScriptVersion + "<br />";
-ChangeLog = ChangeLog + "<br /><b>New: </b>";
-ChangeLog = ChangeLog + "<br />" + "- Added icon scaling so you can adjust the size of the icons";
+//ChangeLog = ChangeLog + "<br /><b>New: </b>";
+//ChangeLog = ChangeLog + "<br />" + "- Added icon scaling so you can adjust the size of the icons";
 ChangeLog = ChangeLog + "<br /><br /><b>Updated: </b>";
-ChangeLog = ChangeLog + "<br />" + "- Small code refactor to support script options";
-ChangeLog = ChangeLog + "<br />" + "- Small code cleanup to avoid duplicate code";
+ChangeLog = ChangeLog + "<br />" + "- Fixed entry for Panama";
 
 // Add Google Varela Round font to make sure signs look the same everywhere (less hassle)
 WebFontConfig = {google:{families:['Varela+Round::latin' ]}};
@@ -130,7 +129,7 @@ var signConfig = {
   RQ:         {'sgn': BGi, 'ann':'mph', 'spd':[ 5,10,15,20,25,30,35,40,45,50,55,60,65]}, //------------------------------- 64.Puerto Rico
   AG:         {'sgn': BGa, 'ann':'kph', 'spd':[ 30, 40, 50, 60, 70, 80, 100, 120]}, //------------------------------------ 65.Algeria
   BX:         {'sgn': BGa, 'ann':'mph', 'spd':[ 30, 35, 40, 50, 60, 70, 80, 90, 110]}, //--------------------------------- 66.Brunei
-  PA:         {'sgn': BGa, 'ann':'mph', 'spd':[ 20, 25, 30, 40, 50, 60, 80, 90, 100, 110]}, //---------------------------- 67.Panama
+  PM:         {'sgn': BGa, 'ann':'kph', 'spd':[ 10, 20, 30, 40, 60, 80]}, //---------------------------------------------- 67.Panama
   UZ:         {'sgn': BGa, 'ann':'kph', 'spd':[ 10, 30, 50, 70, 100]}, //------------------------------------------------- 68.Uzbekistan
   MD:         {'sgn': BGa, 'ann':'kph', 'spd':[ 30, 40, 50, 60, 70, 80, 90]}, // ----------------------------------------- 69.Moldova
   IN:         {'sgn': BGa, 'ann':'kph', 'spd':[ 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130]}, // ------------- 70.India
